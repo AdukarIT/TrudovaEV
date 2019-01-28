d40c654626f1541459076ac0c2435f43369f31bb
Katsiaryna <44376447+ktrudava@users.noreply.github.com>
2019-01-28 20:51:24 +0300

    Add files via upload

---
+++ massives.js
@@ -0,0 +1,27 @@
+// Task 1
+
+var evenNumbers = [];
+for (var i=2; i<=20; i++) {
+	if (i%2===0) {
+		evenNumbers.push(i);
+	}
+}
+console.log(evenNumbers);
+
+// Task 2
+
+var evenNumbers = [];
+for (var i=2; i<=20; i++) {
+	if (i%2===0) {
+		evenNumbers.push(i);
+	}
+}
+console.log(evenNumbers);
+
+for (var i=0; i<evenNumbers.length; i++) {
+	evenNumbers[i]=i*5;
+}
+console.log(evenNumbers);
+
+// Task 3
+
